f381dfad13e67b7be46eba08534def8290cff067
yoni13 <yoni980807@gmail.com>
2023-01-30 00:07:29 +0800

    push

---
+++ src/main.js
@@ -0,0 +1,23 @@
+console.log('Hanlin Cloud Quiz Helper injected!');
+var now = 0;//we procced 0 question
+setTimeout(main, 10000);//sleep 10s
+function main() {
+    console.log('Hanlin Cloud Quiz Helper loaded!');
+    try {
+        while (1){
+            now++;
+            var link = document.querySelector("body > div.container.main-container > div.question-block > div:nth-child("+now+") > iframe").src;
+            var element = document.querySelector("body > div.container.main-container > div.question-block > div:nth-child("+now+") > iframe")
+            console.log(link);
+            // fetch(link, {})
+            // .then((response) => {
+            //     console.log(response);
+            //     return response.json(); 
+            // }).then((data) => {
+            // })
+            // not finished
+        }
+    } catch (e) {
+        console.log('Hanlin Cloud Quiz Helper error: ' + e);
+    }
+}
